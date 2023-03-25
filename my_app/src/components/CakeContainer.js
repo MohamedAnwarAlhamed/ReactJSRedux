b2f9b7370a5a953b31fd3c98708d87536f11944e
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-26 01:35:56 +0300

    React Redux1

---
+++ my_app/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CAKECONTAINER = () => {
+  return (
+    <div>
+      <h2>No. Of Cakes - </h2>
+      <button>Buy Cake</button>
+    </div>
+  )
+}
+
+export default CAKECONTAINER
