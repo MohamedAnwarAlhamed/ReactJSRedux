a8dd83aedef192eda17e0a10560a49b9dd717e63
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-27 00:57:50 +0300

    React Redux9

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -4,10 +4,13 @@ import HOOKSCAKECONTAINER from './components/HooksCakeContainer';
 import ICECREAMCONTAINER from './components/IceCreamContainer';
 import HOOKSICECREAMCONTAINER from './components/HooksIceCreamContainer';
 import NEWCAKECONTAINER from './components/NewCakeContainer';
+import ITEMCONTAINER from './components/ItemContainer';
 
 function App() {
   return (
     <div className="App">
+      <ITEMCONTAINER  cake/>
+      ##################################
       <CAKECONTAINER />
       <HOOKSCAKECONTAINER/>
       ##################################
