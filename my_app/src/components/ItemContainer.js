400747de290f4c47c95243723d12928367c09931
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-27 00:59:40 +0300

    React Redux10

--- my_app/src/components/ItemContainer.js
+++ my_app/src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyCake} from "../redux/cake/cakeAction";
+import { buyIceCream } from "../redux/iceCream/iceCreamAction";
 function ITEMCONTAINER(props) {
   return (
     <div>
       <h2> Item - {props.item}</h2>
-      
+      <button onClick={props.buyItem}>Buy Items</button>
     </div>
   );
 }
@@ -19,13 +21,13 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-// const mapDispatchToProps = (dispatch, ownProps) => {
-//   const dispatchFunction = ownProps.cake
-//     ? () => dispatch(buyCake())
-//     : () => dispatch(buyIceCream());
-//   return {
-//     buyItem: dispatchFunction
-//   };
-// };
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+  return {
+    buyItem: dispatchFunction
+  };
+};
 
-export default connect(mapStateToProps)(ITEMCONTAINER);
+export default connect(mapStateToProps, mapDispatchToProps)(ITEMCONTAINER);
